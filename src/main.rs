20c57c8f1e53c2d3f30bf3cdd99aedcc6711aa5a
Bobby Powers <bobbypowers@gmail.com>
2016-10-14 14:56:31 -0400

    main: work on constraint generation

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::{env, fmt, error, result};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::collections::HashMap;
 
 const EXIT_FAILURE: i32 = 1;
 
@@ -19,6 +20,7 @@ mod implicit;
 mod explicit;
 
 use tok::Tokenizer;
+use explicit::Typ;
 
 // from https://stackoverflow.com/questions/27588416/how-to-send-output-to-stderr
 macro_rules! eprintln(
@@ -128,19 +130,22 @@ struct MVEnv<'a> {
 
 impl<'a> MVEnv<'a> {
     fn new(env: &'a str) -> MVEnv<'a> {
-        MVEnv{env_id: env, next_id: 1}
+        MVEnv {
+            env_id: env,
+            next_id: 1,
+        }
     }
 
-    fn alloc(&mut self, s: &String) -> explicit::Typ {
+    fn alloc(&mut self, s: &String) -> Typ {
         let id = self.next_id;
         self.next_id += 1;
-        explicit::Typ::TMetavar((id, s.clone()))
+        Typ::TMetavar((id, s.clone()))
     }
 
-    fn alloc_empty(&mut self) -> explicit::Typ {
+    fn alloc_empty(&mut self) -> Typ {
         let id = self.next_id;
         self.next_id += 1;
-        explicit::Typ::TMetavar((id, String::from(self.env_id)))
+        Typ::TMetavar((id, String::from(self.env_id)))
     }
 }
 
@@ -149,75 +154,84 @@ fn add_metavars_in(env: &mut MVEnv, exp: &implicit::Exp) -> explicit::Exp {
     use explicit::Exp as E;
 
     match *exp {
-        I::Var(ref id) => {
-            E::Var(id.clone())
-        },
+        I::Var(ref id) => E::Var(id.clone()),
         I::Const(ref c) => E::Const(*c),
         I::Op2(ref op, ref l, ref r) => {
             let el = box add_metavars_in(env, l);
             let er = box add_metavars_in(env, r);
             E::Op2(*op, el, er)
-        },
+        }
         I::Fun(ref id, ref e) => {
             let ee = box add_metavars_in(env, e);
             let mv = env.alloc(id);
             E::Fun(id.clone(), mv, ee)
-        },
+        }
         I::App(ref e1, ref e2) => {
             let ee1 = box add_metavars_in(env, e1);
             let ee2 = box add_metavars_in(env, e2);
             E::App(ee1, ee2)
-        },
+        }
         I::If(ref e1, ref e2, ref e3) => {
             let ee1 = box add_metavars_in(env, e1);
             let ee2 = box add_metavars_in(env, e2);
             let ee3 = box add_metavars_in(env, e3);
             E::If(ee1, ee2, ee3)
-        },
+        }
         I::Let(ref id, ref e1, ref e2) => {
             let ee1 = box add_metavars_in(env, e1);
             let ee2 = box add_metavars_in(env, e2);
             E::Let(id.clone(), ee1, ee2)
-        },
+        }
         I::Fix(ref id, ref e) => {
             let ee = box add_metavars_in(env, e);
             let mv = env.alloc(id);
             E::Fix(id.clone(), mv, ee)
-        },
-        I::Empty => {
-            E::Empty(env.alloc_empty())
-        },
+        }
+        I::Empty => E::Empty(env.alloc_empty()),
         I::Cons(ref hd, ref tl) => {
             let ehd = box add_metavars_in(env, hd);
             let etl = box add_metavars_in(env, tl);
             E::Cons(ehd, etl)
-        },
-        I::Head(ref e) => {
-            E::Head(box add_metavars_in(env, e))
-        },
-        I::Tail(ref e) => {
-            E::Tail(box add_metavars_in(env, e))
-        },
-        I::IsEmpty(ref e) => {
-            E::IsEmpty(box add_metavars_in(env, e))
-        },
+        }
+        I::Head(ref e) => E::Head(box add_metavars_in(env, e)),
+        I::Tail(ref e) => E::Tail(box add_metavars_in(env, e)),
+        I::IsEmpty(ref e) => E::IsEmpty(box add_metavars_in(env, e)),
         I::Pair(ref l, ref r) => {
             let el = box add_metavars_in(env, l);
             let er = box add_metavars_in(env, r);
             E::Pair(el, er)
-        },
-        I::ProjL(ref e) => {
-            E::ProjL(box add_metavars_in(env, e))
-        },
-        I::ProjR(ref e) => {
-            E::ProjR(box add_metavars_in(env, e))
-        },
+        }
+        I::ProjL(ref e) => E::ProjL(box add_metavars_in(env, e)),
+        I::ProjR(ref e) => E::ProjR(box add_metavars_in(env, e)),
     }
 }
 
 fn add_metavars(exp: &implicit::Exp) -> Box<explicit::Exp> {
     let mut env = MVEnv::new("α");
     box add_metavars_in(&mut env, exp)
+}
+
+fn gen_constraints(m: &mut MVEnv,
+                   env: &mut HashMap<&str, &str>,
+                   exp: &explicit::Exp)
+                   -> (Vec<(Typ, Typ)>, Typ) {
+    use common::{Const, Op2};
+    use explicit::Exp as E;
+
+    match *exp {
+        E::Const(Const::Int(_)) => (Vec::new(), Typ::TInt),
+        E::Const(Const::Bool(_)) => (Vec::new(), Typ::TBool),
+        E::Op2(op, ref e1, ref e2) => {
+            let opty = match op {
+                Op2::LT | Op2::GT | Op2::Eq => Typ::TBool,
+                Op2::Add | Op2::Sub | Op2::Mul => Typ::TInt,
+            };
+            let (c1, t1) = gen_constraints(m, env, e1);
+            let (c2, t2) = gen_constraints(m, env, e2);
+            (Vec::new(), Typ::TBool)
+        }
+        _ => (Vec::new(), Typ::TMetavar((1, String::from("a")))),
+    }
 }
 
 fn main() {
@@ -235,6 +249,9 @@ fn main() {
     };
 
     let typed_w_metavars = add_metavars(&exp);
+    let mut cg_env = MVEnv::new("β");
+    let mut id_env = HashMap::new();
+    let constraints = gen_constraints(&mut cg_env, &mut id_env, &typed_w_metavars);
 
     // α β
 
